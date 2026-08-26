48cef539e80139dfea9ae1fa8410bc4d6447b954
Duckly <realactethx@gmail.com>
2026-08-26 23:05:19 +0300

    Update PacksInfo.js

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -3,7 +3,7 @@ export default {
         <h3>About Conix Packs</h3>
         <div class="right-text">
             <p>Packs are sets of levels that conix has made. They range from simple easy challenges to some of the hardest challenges you will ever see!).</p>
-            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!  Furthermore, you can send a screenshot of your list profile in our Discord server, and we will give you the roles for the packs you've completed!</p>
+            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!</p>
         </div>
     `
 }
