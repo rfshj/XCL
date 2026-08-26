d07fcc9bda8eb0abb622ff123ee9c6c60aa03509
Duckly <realactethx@gmail.com>
2026-08-26 23:05:49 +0300

    Update PacksInfo.js

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -2,7 +2,7 @@ export default {
     template: `
         <h3>About Conix Packs</h3>
         <div class="right-text">
-            <p>Packs are sets of levels that conix has made. They range from simple easy challenges to some of the hardest challenges you will ever see!).</p>
+            <p>Packs are sets of levels that share a characteristic. They range from simple easy challenges to some of the hardest challenges you will ever see!).</p>
             <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!</p>
         </div>
     `
